5fa2c0822eb516cf842841eb59719c498250dcfb
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2025-03-29 17:08:18 +0530

    Installed terraform

--- infra/setup.sh
+++ infra/setup.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-#Add Jenkins repo
 yum update -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install yum-utils -y
+
+#Add repo
+  #Jenkins
+  wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+  #Terraform
+  yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
 yum upgrade
+
 
 #Install
   #Dependencies
-dnf install git java-17-amazon-corretto -y
-  #Jenkins & Docker
-yum install jenkins docker -y
+  dnf install git java-17-amazon-corretto -y
+  #Jenkins, Docker, Terraform
+  yum install jenkins docker terraform -y
 
 
 #Start service
   #Jenkins
-systemctl enable jenkins
-systemctl start jenkins
+  systemctl enable jenkins
+  systemctl start jenkins
   #docker 
-service docker start
-usermod -a -G docker ec2-user
-chmod 666 /var/run/docker.sock
+  service docker start
+  usermod -a -G docker ec2-user
+  chmod 666 /var/run/docker.sock
